c65c3b3f3ce42ec5ebeeeef9831acd22971f7951
BenGilbert98 <73166552+BenGilbert98@users.noreply.github.com>
2020-12-18 12:56:36 +0000

    flask app.py with input test

--- app_files/Flask/app.py
+++ app_files/Flask/app.py
@@ -6,8 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def front_page():
-    df = pd.read_csv("~Downloads\ItJobsWatchTop30.csv")
+    file_location = input("Enter file location")
+    df = pd.read_csv(f"{file_location}/ItJobsWatchTop30.csv", encoding= 'unicode_escape')
     return df.to_html()
+
 
 if __name__ == "__main__":
     app.run()
